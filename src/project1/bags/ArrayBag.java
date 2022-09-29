36c767811b791898c997ba83d4726483e2169e36
Richard Pacheco <richard.a.pacheco.iii@gmail.com>
2022-09-28 20:55:25 -0700

    added code for intersection for both bags

--- src/project1/bags/ArrayBag.java
+++ src/project1/bags/ArrayBag.java
@@ -9,12 +9,12 @@ public class ArrayBag<T> implements BagInterface<T> {
 	private int numberOfEntries;
 	private boolean integrityOK = false;
 	private static final int MAX_CAPACITY = 10000;
-	
+
 	public ArrayBag() {
 		this(DEFAULT_CAPCITY);
-		
+
 	}
-	
+
 	public ArrayBag(int capacity) {
 		if (capacity <= MAX_CAPACITY) {
 			@SuppressWarnings("unchecked")
@@ -26,18 +26,19 @@ public class ArrayBag<T> implements BagInterface<T> {
 			throw new IllegalStateException("Attempted to create a bag whose capacity exceeds allowed maximum.");
 		}
 	}
-	
+
 	private void checkIntegrity() {
-		if (!integrityOK) throw new SecurityException("ArrayBag object is corrupt");
+		if (!integrityOK)
+			throw new SecurityException("ArrayBag object is corrupt");
 	}
-	
+
 	/**
 	 * Returns the current size of this bag
 	 */
 	public int getCurrentSize() {
 		return numberOfEntries;
 	}
-	
+
 	/**
 	 * Get whether this bag is Empty
 	 * 
@@ -46,9 +47,10 @@ public class ArrayBag<T> implements BagInterface<T> {
 	public boolean isEmpty() {
 		return numberOfEntries == 0;
 	}
-	
+
 	/**
 	 * Add an entry to this bag.
+	 * 
 	 * @return True if successful; False if unsuccessful.
 	 */
 	public boolean add(T newEntry) {
@@ -59,23 +61,25 @@ public class ArrayBag<T> implements BagInterface<T> {
 		}
 		bag[numberOfEntries] = newEntry;
 		numberOfEntries++;
-		
+
 		return wasAdded;
 	}
-	
+
 	private void checkCapacity(int capacity) {
 		if (capacity > MAX_CAPACITY)
-			throw new IllegalStateException("Attempted to make a bag whose capacity exceeds allowed max of " + MAX_CAPACITY);
+			throw new IllegalStateException(
+					"Attempted to make a bag whose capacity exceeds allowed max of " + MAX_CAPACITY);
 	}
-	
+
 	private void doubleCapacity() {
 		int newLength = 2 * bag.length;
 		checkCapacity(newLength);
-      	this.bag = Arrays.copyOf(bag, newLength);
+		this.bag = Arrays.copyOf(bag, newLength);
 	}
 
 	/**
 	 * Removes an unspecified entry in the bag (The last one/most recent entry)
+	 * 
 	 * @return True if successful; False if unsuccessful
 	 */
 	public T remove() {
@@ -90,10 +94,11 @@ public class ArrayBag<T> implements BagInterface<T> {
 		T result = removeEntry(index);
 		return entry.equals(result); // if the removed item is equal to the one in the argument, return true
 	}
-	
+
 	/**
-	 * A helper method that removes the object at the specified index, 
-	 * filling that index with the object in the last index, then making the last index null
+	 * A helper method that removes the object at the specified index,
+	 * filling that index with the object in the last index, then making the last
+	 * index null
 	 * 
 	 * @param index
 	 * @return
@@ -108,7 +113,7 @@ public class ArrayBag<T> implements BagInterface<T> {
 		}
 		return result;
 	}
-	
+
 	/**
 	 * Removes all entries from this bag
 	 */
@@ -116,9 +121,10 @@ public class ArrayBag<T> implements BagInterface<T> {
 		while (!isEmpty())
 			remove();
 	}
-	
+
 	/**
 	 * Tests whether this bag contains a given entry
+	 * 
 	 * @param anEntry : The Entry to locate
 	 * @return True if this bag contains anEntry, false otherwise
 	 */
@@ -126,18 +132,20 @@ public class ArrayBag<T> implements BagInterface<T> {
 		checkIntegrity();
 		return getIndexOf(anEntry) > -1;
 	}
-	
+
 	public int getFrequencyOf(Object anEntry) {
 		checkIntegrity();
 		int count = 0;
 		for (int i = 0; i < numberOfEntries; i++) {
-			if (bag[i].equals(anEntry)) count++;
+			if (bag[i].equals(anEntry))
+				count++;
 		}
 		return count;
 	}
-	
+
 	/**
 	 * Makes a copy of this bag
+	 * 
 	 * @return Copy of this bag
 	 */
 	public T[] toArray() {
@@ -148,32 +156,45 @@ public class ArrayBag<T> implements BagInterface<T> {
 		}
 		return arrayResult;
 	}
-	
+
 	/**
 	 * Unite two bags together
 	 * Array does not need to be in order; can have empty slots anywhere
+	 * 
 	 * @param arrayToUnite
-	 * @return new bag if union was successful, else throw exception (Null should never be returned)
+	 * @return new bag if union was successful, else throw exception (Null should
+	 *         never be returned)
 	 *
-	public T[] union(T[] arrayToUnite) {
-		checkIntegrity(); // Verify
-		checkCapacity(arrayToUnite.length + this.numberOfEntries); // Check Capacity (Don't actually know numberOfEntries, so worst case is the array has 1 entry)
-		ArrayBag<T> result = this; // Copy of this bag (Contents of both bags should not change)
-			
-		for (int index = 0; index < arrayToUnite.length; index++) { 
-			// Iterate through this array (We use the length instead of numOfEntries, as array can be out of order, like having empty slots before filled ones)
-			if (arrayToUnite[index] != null) // If the current slot is not empty
-				result.add(arrayToUnite[index]); // Add slot contents to the bag (using add method, which will auto-resize bag if new entry doesn't fit)
-		}
-		
-		return result.toArray(); // Can never return this bag w/o changes (Failsafes in place)
-	}*/
-	
+	 *         public T[] union(T[] arrayToUnite) {
+	 *         checkIntegrity(); // Verify
+	 *         checkCapacity(arrayToUnite.length + this.numberOfEntries); // Check
+	 *         Capacity (Don't actually know numberOfEntries, so worst case is the
+	 *         array has 1 entry)
+	 *         ArrayBag<T> result = this; // Copy of this bag (Contents of both bags
+	 *         should not change)
+	 * 
+	 *         for (int index = 0; index < arrayToUnite.length; index++) {
+	 *         // Iterate through this array (We use the length instead of
+	 *         numOfEntries, as array can be out of order, like having empty slots
+	 *         before filled ones)
+	 *         if (arrayToUnite[index] != null) // If the current slot is not empty
+	 *         result.add(arrayToUnite[index]); // Add slot contents to the bag
+	 *         (using add method, which will auto-resize bag if new entry doesn't
+	 *         fit)
+	 *         }
+	 * 
+	 *         return result.toArray(); // Can never return this bag w/o changes
+	 *         (Failsafes in place)
+	 *         }
+	 */
+
 	/**
 	 * Union of two bags using BagInterface<T> instead of an array
 	 * Array does not need to be in order; can have empty slots anywhere
+	 * 
 	 * @param bag
-	 * @return new bag if union was successful, else throw exception (Null should never be returned)
+	 * @return new bag if union was successful, else throw exception (Null should
+	 *         never be returned)
 	 */
 	@Override
 	public BagInterface<T> union(BagInterface<T> bag) {
@@ -181,7 +202,7 @@ public class ArrayBag<T> implements BagInterface<T> {
 		checkCapacity(bag.getCurrentSize() + this.getCurrentSize());
 		ArrayBag<T> result = this;
 		T[] temp = bag.toArray();
-		
+
 		for (int index = 0; index < bag.getCurrentSize(); index++) {
 			if (temp[index] != null) {
 				result.add(temp[index]);
@@ -190,19 +211,29 @@ public class ArrayBag<T> implements BagInterface<T> {
 
 		return result;
 	}
-	
-	
 
-	public T[] intersection(T[] intersectTarget) {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * Returns a bag containing all items found in both bags
+	 * 
+	 * @return
+	 * 
+	 */
+	public BagInterface<T> intersection(BagInterface<T> givenBag) {
+		BagInterface<T> newBag = new ArrayBag<T>();
+		for (T entry : bag) {
+			if (givenBag.contains(entry)) {
+				newBag.add(entry);
+				givenBag.remove(entry);
+			}
+		}
+		return newBag;
 	}
-	
+
 	public T[] diference(T[] differenceTarget) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	/**
 	 * Get whether this bag is full or not
 	 * 
@@ -211,17 +242,18 @@ public class ArrayBag<T> implements BagInterface<T> {
 	public boolean isFull() {
 		return numberOfEntries == bag.length;
 	}
-	
+
 	/**
 	 * Finds the index of the entry in this bag.
+	 * 
 	 * @param anEntry
 	 * @return Index of entry if found, otherwise -1
 	 */
 	private int getIndexOf(T anEntry) {
 		int posOfEntry = -1;
 		boolean isFound = false;
-		int index =0;
-		
+		int index = 0;
+
 		while (!isFound && (index < numberOfEntries)) {
 			if (anEntry.equals(bag[index])) {
 				isFound = true;
@@ -231,9 +263,9 @@ public class ArrayBag<T> implements BagInterface<T> {
 		}
 		return posOfEntry;
 	}
-	
+
 	/**
-	 * Getter for this bag array. 
+	 * Getter for this bag array.
 	 * <b>Remember that it is final, so it cannot be changed, but its content can
 	 * 
 	 * @return
@@ -242,17 +274,17 @@ public class ArrayBag<T> implements BagInterface<T> {
 		return this.bag;
 	}
 
-	@Override
-	public BagInterface<T> intersection(BagInterface<T> bag) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	// @Override
+	// public BagInterface<T> intersection(BagInterface<T> bag) {
+	// // TODO Auto-generated method stub
+	// return null;
+	// }
 
 	@Override
 	public BagInterface<T> difference(BagInterface<T> bag) {
 
 		BagInterface<T> output = new ArrayBag<T>();
-		T[] bag1 =this.toArray();
+		T[] bag1 = this.toArray();
 		T[] bag2 = bag.toArray();
 
 		return output;
--- src/project1/bags/LinkedBag.java
+++ src/project1/bags/LinkedBag.java
@@ -1,62 +1,78 @@
 package project1.bags;
 
+import java.util.Iterator;
+
 public class LinkedBag<T> implements BagInterface<T> {
 	private Node firstNode;
 	private int numberOfEntries;
-	
+
 	public LinkedBag() {
 		firstNode = null;
 		numberOfEntries = 0;
 	}
-	
-	/** Adds a new entry to this bag.
-    @param newEntry  The object to be added as a new entry.
-    @return  True. */
+
+	/**
+	 * Adds a new entry to this bag.
+	 * 
+	 * @param newEntry The object to be added as a new entry.
+	 * @return True.
+	 */
 	public boolean add(T newEntry) { // OutOfMemoryError possible
 		// Add to beginning of chain:
 		Node newNode = new Node(newEntry);
-		newNode.next = firstNode;  // Make new node reference rest of chain
-	                             // (firstNode is null if chain is empty)        
-		firstNode = newNode;       // New node is at beginning of chain
+		newNode.next = firstNode; // Make new node reference rest of chain
+									// (firstNode is null if chain is empty)
+		firstNode = newNode; // New node is at beginning of chain
 		numberOfEntries++;
-	  
+
 		return true;
 	} // End add
-	
-	/** Retrieves all entries that are in this bag.
-    @return  A newly allocated array of all the entries in this bag. */
+
+	/**
+	 * Retrieves all entries that are in this bag.
+	 * 
+	 * @return A newly allocated array of all the entries in this bag.
+	 */
 	public T[] toArray() {
-	   // The cast is safe because the new array contains null entries.
-	   @SuppressWarnings("unchecked")
-	   T[] result = (T[])new Object[numberOfEntries]; // Unchecked cast
-	   
-	   int index = 0;
-	   Node currentNode = firstNode;
-	   while ((index < numberOfEntries) && (currentNode != null)) {
-	      result[index] = currentNode.data;
-	      index++;
-	      currentNode = currentNode.next;
-	   } // End while
-	   
-	   return result;
+		// The cast is safe because the new array contains null entries.
+		@SuppressWarnings("unchecked")
+		T[] result = (T[]) new Object[numberOfEntries]; // Unchecked cast
+
+		int index = 0;
+		Node currentNode = firstNode;
+		while ((index < numberOfEntries) && (currentNode != null)) {
+			result[index] = currentNode.data;
+			index++;
+			currentNode = currentNode.next;
+		} // End while
+
+		return result;
 	} // End toArray
 
-	/** Sees whether this bag is empty.
-    @return  True if the bag is empty, or false if not. */
+	/**
+	 * Sees whether this bag is empty.
+	 * 
+	 * @return True if the bag is empty, or false if not.
+	 */
 	public boolean isEmpty() {
 		return numberOfEntries == 0;
 	} // end isEmpty
 
-	/** Gets the number of entries currently in this bag.
-    @return  The integer number of entries currently in the bag. */
-	public int getCurrentSize()	{
+	/**
+	 * Gets the number of entries currently in this bag.
+	 * 
+	 * @return The integer number of entries currently in the bag.
+	 */
+	public int getCurrentSize() {
 		return numberOfEntries;
 	} // end getCurrentSize
 
-	/** Removes one unspecified entry from this bag, if possible.
-    @return  Either the removed entry, if the removal
-             was successful, or null. 
-    */
+	/**
+	 * Removes one unspecified entry from this bag, if possible.
+	 * 
+	 * @return Either the removed entry, if the removal
+	 *         was successful, or null.
+	 */
 	public T remove() {
 		T result = null;
 		if (firstNode != null) {
@@ -66,11 +82,11 @@ public class LinkedBag<T> implements BagInterface<T> {
 		}
 		return result;
 	} // end remove
-	
+
 	public Node getRefrenceTo(T anEntry) {
 		boolean found = false;
 		Node currentNode = firstNode;
-		
+
 		while (!found && (currentNode != null)) {
 			if (anEntry.equals(currentNode.getData()))
 				found = true;
@@ -80,14 +96,16 @@ public class LinkedBag<T> implements BagInterface<T> {
 		return currentNode;
 	}
 
-	/** Removes one occurrence of a given entry from this bag.
-    @param anEntry  The entry to be removed.
-    @return  True if the removal was successful, or false otherwise. 
-    */
+	/**
+	 * Removes one occurrence of a given entry from this bag.
+	 * 
+	 * @param anEntry The entry to be removed.
+	 * @return True if the removal was successful, or false otherwise.
+	 */
 	public boolean remove(T anEntry) {
 		boolean result = false;
 		Node nodeN = getRefrenceTo(anEntry);
-		
+
 		if (nodeN != null) {
 			nodeN.setData(firstNode.getData());
 			firstNode = firstNode.getNextNode();
@@ -96,31 +114,38 @@ public class LinkedBag<T> implements BagInterface<T> {
 		}
 		return result;
 	} // end remove
-	
+
 	/** Removes all entries from this bag. */
 	public void clear() {
 		while (!isEmpty())
 			remove();
 	} // end clear
-	
-	/** Counts the number of times a given entry appears in this bag.
-		 @param anEntry  The entry to be counted.
-		 @return  The number of times anEntry appears in the bag. */
+
+	/**
+	 * Counts the number of times a given entry appears in this bag.
+	 * 
+	 * @param anEntry The entry to be counted.
+	 * @return The number of times anEntry appears in the bag.
+	 */
 	public int getFrequencyOf(T anEntry) {
 		int frequency = 0;
 		int counter = 0;
 		Node currentNode = firstNode;
 		while ((counter < numberOfEntries) && (currentNode != null)) {
-			if (anEntry.equals(currentNode.getData())) frequency++;
+			if (anEntry.equals(currentNode.getData()))
+				frequency++;
 			counter++;
 			currentNode = currentNode.getNextNode();
 		}
 		return frequency;
 	} // End getFrequencyOf
-	
-	/** Tests whether this bag contains a given entry.
-		 @param anEntry  The entry to locate.
-		 @return  True if the bag contains anEntry, or false otherwise. */
+
+	/**
+	 * Tests whether this bag contains a given entry.
+	 * 
+	 * @param anEntry The entry to locate.
+	 * @return True if the bag contains anEntry, or false otherwise.
+	 */
 	public boolean contains(T anEntry) {
 		boolean found = false;
 		Node currentNode = firstNode;
@@ -136,23 +161,25 @@ public class LinkedBag<T> implements BagInterface<T> {
 	/**
 	 * Unites 2 LinkedBags together using Arrays
 	 * Array does need to be in order; Can have null/empty spots
+	 * 
 	 * @param unionTarget the array to unite with this one
 	 * @return New bag as an array, else this LinkedBag is returned
 	 *
-	public T[] union(T[] unionTarget) {
-		LinkedBag<T> result = this;
-		
-		for (int index = 0; index < unionTarget.getCurrentSize(); index++) {
-			if (unionTarget[index] != null)
-				result.add(unionTarget[index]);
-		}
-		return result.toArray();
-	}
-	*/
-	
+	 *         public T[] union(T[] unionTarget) {
+	 *         LinkedBag<T> result = this;
+	 * 
+	 *         for (int index = 0; index < unionTarget.getCurrentSize(); index++) {
+	 *         if (unionTarget[index] != null)
+	 *         result.add(unionTarget[index]);
+	 *         }
+	 *         return result.toArray();
+	 *         }
+	 */
+
 	/**
 	 * Unites 2 LinkedBags together using BagInterface
 	 * Array does need to be in order; Can have null/empty spots
+	 * 
 	 * @param bag the array to unite with this one
 	 * @return New bag as an array, else this LinkedBag is returned
 	 */
@@ -160,7 +187,7 @@ public class LinkedBag<T> implements BagInterface<T> {
 	public BagInterface<T> union(BagInterface<T> bag) {
 		LinkedBag<T> result = this;
 		T[] temp = bag.toArray();
-		
+
 		for (int index = 0; index < bag.getCurrentSize(); ++index) {
 			if (temp[index] != null) {
 				result.add(temp[index]);
@@ -169,12 +196,20 @@ public class LinkedBag<T> implements BagInterface<T> {
 		return result;
 	}
 
-	public T[] intersection(T[] intersectTarget) {
-		// TODO Auto-generated method stub
-		return null;
+	public BagInterface<T> intersection(BagInterface<T> intersectTarget) {
+		T[] tempArr = toArray();
+		BagInterface<T> newBag = new LinkedBag<T>();
+
+		for (T entry : tempArr) {
+			if (intersectTarget.contains(entry)) {
+				newBag.add(entry);
+				intersectTarget.remove(entry);
+			}
+		}
+		return newBag;
 	}
 
-	public T[] diference(T[] differenceTarget) {
+	public T[] difference(T[] differenceTarget) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -182,28 +217,28 @@ public class LinkedBag<T> implements BagInterface<T> {
 	class Node {
 		private T data;
 		private Node next;
-		
+
 		private Node(T dataPortion) {
-			this (dataPortion, null);
+			this(dataPortion, null);
 		}
-		
+
 		private Node(T dataPortion, Node nextNode) {
 			data = dataPortion;
 			next = nextNode;
 		}
-		
+
 		private T getData() {
 			return data;
 		}
-		
+
 		private void setData(T newData) {
 			data = newData;
 		}
-		
+
 		private Node getNextNode() {
 			return next;
 		}
-		
+
 		@SuppressWarnings("unused")
 		private void setNextNode(Node nextNode) {
 			next = nextNode;
@@ -215,11 +250,11 @@ public class LinkedBag<T> implements BagInterface<T> {
 		throw new UnsupportedOperationException("Method not supported");
 	}
 
-	@Override
-	public BagInterface<T> intersection(BagInterface<T> bag) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	// @Override
+	// public BagInterface<T> intersection(BagInterface<T> bag) {
+	// // TODO Auto-generated method stub
+	// return null;
+	// }
 
 	@Override
 	public BagInterface<T> difference(BagInterface<T> bag) {
